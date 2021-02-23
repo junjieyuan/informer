33da95447e4f45fdad8480441ede16993099ab6b
Junjie Yuan <yuan@junjie.pro>
2021-02-23 16:16:05 +0800

    Implements API: Login
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

--- api/server.go
+++ api/server.go
@@ -6,19 +6,97 @@ import (
 	"github.com/gorilla/mux"
 	"io"
 	"io/ioutil"
+	"junjie.pro/informer/conf"
 	"junjie.pro/informer/library"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Serve() {
 	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/login", Login)
+
 	router.HandleFunc("/libraries", List)
 	router.HandleFunc("/libraries/add", Add)
 	router.HandleFunc("/libraries/remove", Remove)
 	router.HandleFunc("/libraries/update", Update)
 
 	log.Fatalln(http.ListenAndServe(":8080", router))
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user conf.User
+
+	body, err := ioutil.ReadAll(io.Reader(r.Body))
+	if err != nil {
+		panic(err)
+	}
+
+	err = r.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(500)
+		err = json.NewEncoder(w).Encode(err)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	informerConfig, err := conf.ReadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if informerConfig.CheckUser(user) {
+		tokenId := conf.GenerateToken()
+		createDate := time.Now()
+		token := conf.Token{ID: tokenId, CreateDate: createDate}
+		expire := time.Now().AddDate(0, 0, informerConfig.RenewalCycle)
+
+		informerConfig.User.AddToken(token)
+		usernameCookie := http.Cookie{
+			Name:       "username",
+			Value:      user.Username,
+			Path:       "",
+			Domain:     "",
+			Expires:    expire,
+			RawExpires: "",
+			MaxAge:     0,
+			Secure:     false,
+			HttpOnly:   true,
+			SameSite:   0,
+			Raw:        "",
+			Unparsed:   nil,
+		}
+		http.SetCookie(w, &usernameCookie)
+		tokenCookie := http.Cookie{
+			Name:       "token",
+			Value:      tokenId,
+			Path:       "",
+			Domain:     "",
+			Expires:    expire,
+			RawExpires: "",
+			MaxAge:     0,
+			Secure:     false,
+			HttpOnly:   true,
+			SameSite:   0,
+			Raw:        "",
+			Unparsed:   nil,
+		}
+		http.SetCookie(w, &tokenCookie)
+
+		err := informerConfig.WriteConfig()
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func List(w http.ResponseWriter, r *http.Request) {
