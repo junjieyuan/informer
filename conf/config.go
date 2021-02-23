bae362c32ea3fc2f1fbda30423f6821fe7c2cbe2
Junjie Yuan <yuan@junjie.pro>
2021-02-23 16:14:30 +0800

    add code of read/modify Informer configuration file
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

---
+++ conf/config.go
@@ -0,0 +1,105 @@
+package conf
+
+import (
+	"encoding/hex"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/sha3"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type InformerConfig struct {
+	Version      string `yaml:"version"`
+	RenewalCycle int    `yaml:"renewal-cycle"`
+	User         User   `yaml:"user"`
+}
+
+type User struct {
+	Username string  `json:"username" yaml:"username"`
+	Password string  `json:"password" yaml:"password"`
+	Tokens   []Token `json:"tokens" yaml:"tokens"`
+}
+
+type Token struct {
+	ID         string    `json:"id" yaml:"id"`
+	CreateDate time.Time `json:"create-date" yaml:"create-date"`
+}
+
+func ReadConfig() (InformerConfig, error) {
+	configLocation, err := configPath()
+	informerConfig := InformerConfig{}
+
+	if err != nil {
+		return informerConfig, err
+	}
+
+	configFile, err := ioutil.ReadFile(configLocation)
+	if err != nil {
+		return informerConfig, err
+	}
+
+	err = yaml.Unmarshal(configFile, &informerConfig)
+	if err != nil {
+		return InformerConfig{}, err
+	}
+
+	return informerConfig, nil
+}
+
+func (informerConfig InformerConfig) WriteConfig() error {
+	dataLocation, err := configPath()
+	if err != nil {
+		return err
+	}
+
+	data, err := yaml.Marshal(informerConfig)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(dataLocation, data, os.FileMode(0600))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (informerConfig InformerConfig) CheckUser(user User) bool {
+	//covert [N]byte to []byte, then covert []byte to hex string, same as sha3sum command
+	digest := sha3.Sum512([]byte(user.Password))
+	user.Password = hex.EncodeToString(digest[:])
+
+	if informerConfig.User.Username == user.Username && informerConfig.User.Password == user.Password {
+		return true
+	}
+
+	return false
+}
+
+func (user *User) AddToken(token Token) {
+	user.Tokens = append(user.Tokens, token)
+}
+
+func configPath() (string, error) {
+	configPath := os.Getenv("XDG_CONFIG_HOME")
+	if configPath == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+
+		configPath = strings.Join([]string{homeDir, ".config"}, string(filepath.Separator))
+	}
+	location := strings.Join([]string{configPath, "informer", "config.yaml"}, string(filepath.Separator))
+
+	return location, nil
+}
+
+func GenerateToken() string {
+	return uuid.NewString()
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module junjie.pro/informer
 go 1.15
 
 require (
+	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 	gopkg.in/yaml.v2 v2.4.0
 )
