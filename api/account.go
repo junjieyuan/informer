6b0d5b83fec36955ec19bee66aa3bda1708b8855
Junjie Yuan <yuan@junjie.pro>
2021-03-22 09:50:00 +0800

    split Login from server.go
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

---
+++ api/account.go
@@ -0,0 +1,135 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"junjie.pro/informer/conf"
+	"log"
+	"net/http"
+	"time"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	//Response message is json
+	w.Header().Add("Content-Type", "application/json")
+
+	//Read request body and close it
+	body, err := ioutil.ReadAll(io.Reader(r.Body))
+	if err != nil {
+		w.WriteHeader(500)
+		log.Fatalln(err)
+	}
+	err = r.Body.Close()
+	if err != nil {
+		w.WriteHeader(500)
+		log.Fatalln(err)
+	}
+
+	//Read informer configurations
+	informerConfig, err := conf.ReadConfig()
+	if err != nil {
+		w.WriteHeader(500)
+		log.Fatalln(err)
+	}
+
+	//Read login token from cookie
+	username, err := r.Cookie("username")
+	if err != nil {
+		log.Println(err)
+	}
+	tokenId, err := r.Cookie("token")
+	if err != nil {
+		log.Println(err)
+	}
+
+	//Check user is already logged in whether
+	if username != nil && tokenId != nil && informerConfig.CheckLogin(username.Value, tokenId.Value) {
+		w.WriteHeader(200)
+		message := fmt.Sprintf(messageTemplate, "success")
+		err = json.NewEncoder(w).Encode(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		return
+	}
+
+	//Parse user login information from request body
+	var user conf.User
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		w.WriteHeader(500)
+		message := fmt.Sprintf(messageTemplate, err.Error())
+		err = json.NewEncoder(w).Encode(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		return
+	}
+
+	//If login information correctly and successfully login, return 200 success,
+	//else return 401 login information not correctly or 500
+	if informerConfig.CheckUser(user) {
+		tokenId := conf.GenerateToken()
+		createDate := time.Now()
+		token := conf.Token{ID: tokenId, CreateDate: createDate}
+		expire := time.Now().AddDate(0, 0, informerConfig.RenewalCycle)
+
+		//Save token
+		informerConfig.User.AddToken(token)
+		err = informerConfig.WriteConfig()
+		if err != nil {
+			w.WriteHeader(500)
+			log.Fatalln(err)
+		}
+
+		//Set cookie: username and token
+		usernameCookie := http.Cookie{
+			Name:       "username",
+			Value:      user.Username,
+			Path:       "",
+			Domain:     "",
+			Expires:    expire,
+			RawExpires: "",
+			MaxAge:     0,
+			Secure:     false,
+			HttpOnly:   true,
+			SameSite:   0,
+			Raw:        "",
+			Unparsed:   nil,
+		}
+		http.SetCookie(w, &usernameCookie)
+		tokenCookie := http.Cookie{
+			Name:       "token",
+			Value:      tokenId,
+			Path:       "",
+			Domain:     "",
+			Expires:    expire,
+			RawExpires: "",
+			MaxAge:     0,
+			Secure:     false,
+			HttpOnly:   true,
+			SameSite:   0,
+			Raw:        "",
+			Unparsed:   nil,
+		}
+		http.SetCookie(w, &tokenCookie)
+
+		w.WriteHeader(200)
+		message := fmt.Sprintf(messageTemplate, "success")
+		err = json.NewEncoder(w).Encode(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		w.WriteHeader(401)
+		message := fmt.Sprintf(messageTemplate, "username or password not correctly")
+		err = json.NewEncoder(w).Encode(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
